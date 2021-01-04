973a1e932a723576d8a16a791dbb19ef66d14c32
kalyanshivakumar <kalyanshivakumar998@gmail.com>
2021-01-04 20:16:05 +0530

    update

---
+++ age.js
@@ -0,0 +1,15 @@
+var age=55;
+function f1(){
+console.log("this is the age of"+age);
+
+}
+function f2(){
+    console.log("this is the age of"+age);
+}
+function f3(){
+
+console.log("this is the age of"+age);
+}
+f1();
+f2();
+f3();
